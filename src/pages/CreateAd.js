9df9e4eab29e366d6eb45afc88aa87dfbcbdb811
joaovitorcostaa <joao.vitoramc@gmail.com>
2021-03-30 11:53:58 -0300

    Correções no início do createAd

--- src/pages/CreateAd.js
+++ src/pages/CreateAd.js
@@ -3,13 +3,13 @@ import React from "react";
 import styled from "styled-components";
 
 const DivContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 const DivInputs = styled.div`
-    border-bottom: 1px solid black;
+  border-bottom: 1px solid black;
 `;
 const Input = styled.input``;
 const DivProducts = styled.div``;
@@ -26,25 +26,20 @@ class CreateAd extends React.Component {
     products: [],
     inputSearchProduct: "",
   };
-  
+
   componentDidMount = () => {
-      this.getProducts()
-  }
+    this.getProducts();
+  };
 
   getProducts = () => {
     axios
-    .get(
-      "https://us-central1-labenu-apis.cloudfunctions.net/eloFourTwo/products",
-      body,
-      {
-        headers: {
-          Authorization: "joao-vitor-alves",
-        },
-      }
-    ).then((res) => {
-        this.setState({products: res.data})
-    })
-  }
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/eloFourTwo/products"
+      )
+      .then((res) => {
+        this.setState({ products: res.data.products });
+      });
+  };
 
   createProduct = () => {
     const body = {
@@ -59,34 +54,33 @@ class CreateAd extends React.Component {
     axios
       .post(
         "https://us-central1-labenu-apis.cloudfunctions.net/eloFourTwo/products",
-        body,
-        {
-          headers: {
-            Authorization: "joao-vitor-alves",
-          },
-        }
+        body
       )
       .then((res) => {
-        this.setState({ inputName: "" });
-        this.setState({ inputDescription: "" });
-        this.setState({ inputPrice: "" });
-        this.setState({ inputPaymentMethod: "" });
-        this.setState({ inputCategory: "" });
-        this.setState({ inputPhotos: "" });
-        this.setState({ inputInstallments: "" });
-        alert("Seu produto já foi anunciado!")
+        this.setState({
+          inputName: "",
+          inputDescription: "",
+          inputPrice: "",
+          inputPaymentMethod: "",
+          inputCategory: "",
+          inputPhotos: "",
+          inputInstallments: "",
+        });
+        alert("Seu produto já foi anunciado!");
       })
       .catch((err) => {
-        this.setState({ inputName: "" });
-        this.setState({ inputDescription: "" });
-        this.setState({ inputPrice: "" });
-        this.setState({ inputPaymentMethod: "" });
-        this.setState({ inputCategory: "" });
-        this.setState({ inputPhotos: "" });
-        this.setState({ inputInstallments: "" });
-        alert("Ocorreu um erro! Estamos trabalhando para solucioná-lo!")
-        console.log(err)
-      })
+        this.setState({
+          inputName: "",
+          inputDescription: "",
+          inputPrice: "",
+          inputPaymentMethod: "",
+          inputCategory: "",
+          inputPhotos: "",
+          inputInstallments: "",
+        });
+        alert("Ocorreu um erro! Estamos trabalhando para solucioná-lo!");
+        console.log(err);
+      });
   };
 
   handleName = (e) => {
@@ -112,55 +106,83 @@ class CreateAd extends React.Component {
   };
 
   deleteProduct = (productId) => {
-    axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/eloFourTwo/products/${productId}`, 
-    {
-        headers: {
-            Authorization: "joao-vitor-alves"
-        }
-    }).then((res) => {
-        alert("Esse produto foi deletado!")
-    })
-  }
+    axios
+      .delete(
+        `https://us-central1-labenu-apis.cloudfunctions.net/eloFourTwo/products/${productId}`
+      )
+      .then((res) => {
+        alert("Esse produto foi deletado!");
+        this.getProducts();
+      });
+  };
 
-
-
-  render(){
+  render() {
     const listOfProducts = this.state.products.map((product) => (
-        <div>
-            <p>{product.name}</p>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <p>{product.paymentMethod}</p>
-            <p>{product.category}</p>
-            <p>{product.photos}</p>
-            <p>{product.installments}</p>
-            <button onClick={() => this.deleteProduct(product.id)}>Apagar produto</button>
-        </div>
-    ))
+      <div>
+        <p>{product.name}</p>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+        <p>{product.paymentMethod}</p>
+        <p>{product.category}</p>
+        <p>{product.photos}</p>
+        <p>{product.installments}</p>
+        <button onClick={() => this.deleteProduct(product.id)}>
+          Apagar produto
+        </button>
+      </div>
+    ));
 
-    return (<DivContainer>
+    return (
+      <DivContainer>
         <DivInputs>
-        <label>Nome do produto:</label>
-        <Input value={this.state.inputName} onChange={this.handleName}> </Input>
-        <label>Descrição do produto</label>
-        <Input value={this.state.inputDescription} onChange={this.handleDescription}> </Input>
-        <label>Preço: </label>
-        <Input value={this.state.inputPrice} onChange={this.handlePrice}> </Input>
-        <label>Métodos de pagamento: </label>
-        <Input value={this.state.inputPaymentMethod} onChange={this.handlePaymentMethod}> </Input>
-        <label>Categoria: </label>
-        <Input value={this.state.inputCategory} onChange={this.handleCategory}> </Input>
-        <label>Fotos: </label>
-        <Input value={this.state.inputPhotos} onChange={this.handlePhotos}> </Input>
-        <label>Parcelas: </label>
-        <Input value={this.state.inputInstallments} onChange={this.handleInstallments}> </Input>
+          <label>Nome do produto:</label>
+          <Input value={this.state.inputName} onChange={this.handleName}>
+            {" "}
+          </Input>
+          <label>Descrição do produto</label>
+          <Input
+            value={this.state.inputDescription}
+            onChange={this.handleDescription}
+          >
+            {" "}
+          </Input>
+          <label>Preço: </label>
+          <Input value={this.state.inputPrice} onChange={this.handlePrice}>
+            {" "}
+          </Input>
+          <label>Métodos de pagamento: </label>
+          <Input
+            value={this.state.inputPaymentMethod}
+            onChange={this.handlePaymentMethod}
+          >
+            {" "}
+          </Input>
+          <label>Categoria: </label>
+          <Input
+            value={this.state.inputCategory}
+            onChange={this.handleCategory}
+          >
+            {" "}
+          </Input>
+          <label>Fotos: </label>
+          <Input value={this.state.inputPhotos} onChange={this.handlePhotos}>
+            {" "}
+          </Input>
+          <label>Parcelas: </label>
+          <Input
+            value={this.state.inputInstallments}
+            onChange={this.handleInstallments}
+          >
+            {" "}
+          </Input>
         </DivInputs>
+
         <DivProducts>
-        <h1>Meus produtos</h1>
-        {listOfProducts}
+          <h1>Meus produtos</h1>
+          {listOfProducts}
         </DivProducts>
-    </DivContainer>
-    )
+      </DivContainer>
+    );
   }
 }
 
