d9460260c50d994c6ea093c5c931d0560faa9abd
samenc99 <mateus.enc@gmail.com>
2021-04-01 14:54:23 -0300

    mudando overflow para auto, para que não tenha scroll horizontal

--- src/pages/CreateAd.js
+++ src/pages/CreateAd.js
@@ -44,7 +44,7 @@ const DivDelete = styled.div`
 display: flex;
 flex-direction: column;
   max-height: 270px;
-  overflow: scroll;
+  overflow: auto;
 div{
   display:flex;
   justify-content: space-between;
