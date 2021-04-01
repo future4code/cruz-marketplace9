706d36566973e58fcb140550d0fa7eb3eaec3605
samenc99 <mateus.enc@gmail.com>
2021-04-01 15:46:03 -0300

    estilização do cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,20 +2,53 @@ import Axios from "axios";
 import React from "react";
 import styled from 'styled-components';
 
+const DivContainer = styled.div`
+    margin-left: 10px;
+    margin-bottom: 5px;
+    padding: 0 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid black;
+    div{
+        :nth-child(1){
+            display: flex;
+            flex-direction:column;
+            align-items: flex-start;
+        }
+        :nth-child(2){
+            p{  
+                text-align: center;
+                border:0;
+                width: 20px;
+                cursor: pointer;
+                :hover{
+                    background-color: #f83940;
+                    opacity: 70%;
+                    border-radius: 5px;
+                }
+            }
+        }
+    }
+`
+
 export default class Cart extends React.Component {
 
 
 
     render (){
         return (
-            <div>
-                <p>{this.props.name}</p>
-                <p>{this.props.amount}x</p>
-                <p>R${this.props.price},00</p>
-                <button onClick={() => this.props.deleteCartProduct(this.props.id)}>
-                    Remover item
-                </button>
-            </div>
+            <DivContainer>
+                <div>
+                    <p><b>{this.props.name}</b>  {this.props.amount}x</p>
+                    <p>R${this.props.price},00</p>
+                </div>
+                <div>
+                    <p onClick={() => this.props.deleteCartProduct(this.props.id)}>
+                        <b>x</b>
+                    </p>
+                </div>
+            </DivContainer>
         )
     }
 }
